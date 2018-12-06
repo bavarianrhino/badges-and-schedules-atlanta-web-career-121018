96b9a100d27d3f2dbca53f18e056eb3c6e56b375
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-12-06 23:54:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator (array)
+  newarr = []
+  array.each do |x|
+    array.push(x)
+  end
+  return newarr
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
